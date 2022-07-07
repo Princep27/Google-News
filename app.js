7cc7fcec2f5fd414a436d4eca870c1c402881d24
Prince Patel <pptwo7@gmail.com>
2022-07-07 22:55:28 +0530

    making necessay changes

--- app.js
+++ app.js
@@ -150,6 +150,6 @@ app.get("/",function(req,res){
 
 
 
-app.listen(3000 || process.env.PORT,function(){
+app.listen(process.env.PORT || 3000 ,function(){
     console.log("server is running");
 });
